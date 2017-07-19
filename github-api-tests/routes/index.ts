3d0130a023241dce4015a93453321edfb6d17292
Nathan Phillips <github@nathanphillips.co.uk>
2017-07-19 11:18:44 +0100

    Updated to latest library Updated version number to match that of the library Added pages to test commits Added page for individual issue

--- github-api-tests/routes/index.ts
+++ github-api-tests/routes/index.ts
@@ -36,6 +36,40 @@ router.get("/repos/:owner/:repo", async (req: express.Request, res: express.Resp
   }
 });
 
+router.get("/repos/:owner/:repo/commits", async (req: express.Request, res: express.Response) => {
+  try {
+    const repo = await gitHub.getUser(req.params.owner).getRepository(req.params.repo).loadAsync();
+    if (repo === null)
+      throw { status: HttpStatusCodes.NotFound, message: "Repository not found" };
+    const commits = await repo.loadCommitsAsync();
+    res.render("commits", { title: `Commits - ${repo.fullName} - Repositories`, repo: repo, commits: commits });
+  } catch (err) {
+    res.status(err.status || HttpStatusCodes.InternalServerError);
+    res.render("error", {
+      message: err.message,
+      error: err,
+    });
+  }
+});
+
+router.get("/repos/:owner/:repo/commits/:sha", async (req: express.Request, res: express.Response) => {
+  try {
+    const repo = await gitHub.getUser(req.params.owner).getRepository(req.params.repo).loadAsync();
+    if (repo === null)
+      throw { status: HttpStatusCodes.NotFound, message: "Repository not found" };
+    const commit = await repo.getCommit(req.params.sha).loadAsync();
+    if (commit === null)
+      throw { status: HttpStatusCodes.NotFound, message: "Commit not found" };
+    res.render("commit", { title: `${commit.sha} - Commits - ${repo.fullName} - Repositories`, repo: repo, commit: commit });
+  } catch (err) {
+    res.status(err.status || HttpStatusCodes.InternalServerError);
+    res.render("error", {
+      message: err.message,
+      error: err,
+    });
+  }
+});
+
 router.get("/repos/:owner/:repo/issues", async (req: express.Request, res: express.Response) => {
   try {
     const repo = await gitHub.getUser(req.params.owner).getRepository(req.params.repo).loadAsync();
@@ -43,6 +77,24 @@ router.get("/repos/:owner/:repo/issues", async (req: express.Request, res: expre
       throw { status: HttpStatusCodes.NotFound, message: "Repository not found" };
     const issues = await repo.loadIssuesAsync("none", "all");
     res.render("issues", { title: `Issues - ${repo.fullName} - Repositories`, repo: repo, issues: issues });
+  } catch (err) {
+    res.status(err.status || HttpStatusCodes.InternalServerError);
+    res.render("error", {
+      message: err.message,
+      error: err,
+    });
+  }
+});
+
+router.get("/repos/:owner/:repo/issues/:number", async (req: express.Request, res: express.Response) => {
+  try {
+    const repo = await gitHub.getUser(req.params.owner).getRepository(req.params.repo).loadAsync();
+    if (repo === null)
+      throw { status: HttpStatusCodes.NotFound, message: "Repository not found" };
+    const issue = await repo.getIssue(req.params.number).loadAsync();
+    if (issue === null)
+      throw { status: HttpStatusCodes.NotFound, message: "Issue not found" };
+    res.render("issue", { title: `#${issue.number} - Issues - ${repo.fullName} - Repositories`, repo: repo, issue: issue });
   } catch (err) {
     res.status(err.status || HttpStatusCodes.InternalServerError);
     res.render("error", {
